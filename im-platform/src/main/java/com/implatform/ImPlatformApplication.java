79f6b89f0ea01e8eee8a5573a9da207fd7637df6
ZhuanZ <3877189298@qq.com>
2026-09-11 08:31:51 +0800

    配置依赖

---
+++ im-platform/src/main/java/com/implatform/ImPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.implatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImPlatformApplication.class, args);
+    }
+
+}
