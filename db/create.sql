630a4fa16ef31fde80fbbbb31c8e730fe75f1b98
ZhuanZ <3877189298@qq.com>
2026-09-14 19:31:15 +0800

    feat:敏感词、消息删除记录表+实体类

--- db/create.sql
+++ db/create.sql
@@ -114,3 +114,23 @@ create table `im_group_message`
     key              `idx_group_id_seq_no` (`group_id`,`seq_no`)
 ) engine = innodb charset = utf8mb4 comment '群消息';
 
+create table `im_message_deletion`
+(
+    `id`          bigint  not null auto_increment primary key comment 'id',
+    `user_id`     bigint  not null comment '用户id',
+    `chat_type`   tinyint not null comment '会话类型 1:私聊 2:群聊',
+    `chat_id`     bigint  not null comment '好友id、群聊id',
+    `message_id`  bigint(20) comment '消息id',
+    `delete_type` tinyint not null comment '删除类型 1:按消息删除 2:按会话删除',
+    `delete_time` datetime default current_timestamp comment '消息删除时间',
+    key           `idx_user_id` (`user_id`)
+) engine = innodb charset = utf8mb4 comment '消息删除记录';
+
+create table `im_sensitive_word`
+(
+    `id`          bigint      not null auto_increment primary key comment 'id',
+    `content`     varchar(64) not null comment '敏感词内容',
+    `enabled`     tinyint(1) default 0 comment '是否启用 0:未启用 1:启用',
+    `creator`     bigint   default null comment '创建者',
+    `create_time` datetime default current_timestamp comment '创建时间'
+) engine = innodb charset = utf8mb4 comment '敏感词';
